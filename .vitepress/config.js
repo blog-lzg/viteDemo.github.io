d30ed799128753036a1fdba2ee550f6e1238f4d5
blog-lzg <1074797410@qq.com>
2024-03-09 10:46:11 +0800

    init

---
+++ .vitepress/config.js
@@ -0,0 +1,12 @@
+// .vitepress/config.js
+module.exports = {
+    title: 'My Docs',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'About', link: '/about/' },
+      ],
+    },
+  };
+  
